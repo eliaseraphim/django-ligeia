ba356e265c457992e6c6d8a21b20f9c13739747d
Elia Deppe <elia.deppe7@gmail.com>
2023-02-22 22:41:10 -0500

    Updated models with new properties, updated fields.

--- music/models.py
+++ music/models.py
@@ -2,23 +2,38 @@ from django.db import models
 from django.utils import timezone
 
 
-def get_artists(query_set):
-    artists = ''
-    for artist in query_set:
-        artists += f'{artist.name}, '
+def get_property_set(query_set: models.QuerySet, relation: str):
+    """
+    Get a ``set`` of values from a given object's property in a query set.
 
-    return artists.strip(', ')
+    :param query_set: The query set to iterate through.
+    :type query_set: django.db.models.QuerySet
+    :param relation: The name of the relation field that you are reading.
+    :type relation: str
+    :returns: A ``set`` of values from a given object's property in a query set.
+    :rtype: set
+    """
+    if not isinstance(query_set, models.QuerySet) or not hasattr(query_set.first(), relation)
+        return None
+
+    items = set()
+    for obj in query_set:
+        for value in getattr(obj, relation):
+            items.add(value)
+
+    return items
 
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(blank=True, verbose_name='Artist Image')
-    albums = models.ManyToManyField('music.Album', blank=True)
-    songs = models.ManyToManyField('music.Songs', blank=True)
-    featured_on = models.ManyToManyField('music.Songs', blank=True, verbose_name='Featured On')
 
     def __str__(self):
         return self.name
+
+    @property
+    def albums(self):
+        return get_property_set(self.songs.all(), 'album')
 
 
 class Album(models.Model):
@@ -30,28 +45,44 @@ class Album(models.Model):
 
     title = models.CharField(max_length=200)
     type = models.CharField(choices=TYPE_CHOICES, max_length=50)
-    artwork = models.ImageField(verbose_name='Album Artwork')
-    duration = models.DurationField()
-    released = models.DateField()
+    artwork = models.ImageField(blank=True, verbose_name='Album Artwork')
+    duration = models.DurationField(default=0)
+    released = models.DateField(blank=True)
 
     def __str__(self):
         return self.title
 
     @property
     def artists(self):
-        return self.songs.all()
+        return get_property_set(self.songs.all(), 'artists')
+
+    @property
+    def features(self):
+        return get_property_set(self.songs.all(), 'features')
 
 
 class Playlist(models.Model):
     title = models.CharField(max_length=200)
-    songs = models.ManyToManyField('music.Song', blank=True, on_delete=models.SET_DEFAULT)
+    songs = models.ManyToManyField('music.Song', blank=True, on_delete=models.SET_DEFAULT, related_name='on_playlists')
     artwork = models.ImageField(verbose_name='Playlist Artwork')
-    duration = models.DurationField()
+    duration = models.DurationField(default=0)
     created = models.DateTimeField(default=timezone.now)
     last_edited = models.DateTimeField(default=timezone.now, verbose_name='Last Edited')
 
     def __str__(self):
         return self.title
+
+    @property
+    def artists(self):
+        return get_property_set(self.songs.all(), 'artists')
+
+    @property
+    def features(self):
+        return get_property_set(self.songs.all(), 'features')
+
+    @property
+    def albums(self):
+        return get_property_set(self.songs.all(), 'album')
 
 
 class Song(models.Model):
@@ -59,11 +90,8 @@ class Song(models.Model):
     artists = models.ManyToManyField('music.Artist', blank=True, verbose_name='Artist(s)', related_name='songs')
     features = models.ManyToManyField('music.Artist', blank=True, verbose_name='Feature(s)', related_name='featured_on')
     album = models.ForeignKey('music.Album', blank=True, on_delete=models.SET_DEFAULT, related_name='songs')
-    duration = models.DurationField()
-    released = models.DateField(blank=True, null=True)
+    duration = models.DurationField(default=0)
+    released = models.DateField(blank=True)
 
     def __str__(self):
         return self.title
-
-    def get_artists(self):
-        return get_artists(self.artists.all())
